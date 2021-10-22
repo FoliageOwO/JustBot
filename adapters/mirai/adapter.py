c51b9158dc3106a8ea70b4af2dfe306540351467
WindLeaf233 <mc.windleaf@foxmail.com>
2021-10-22 15:40:14 +0800

    同步 MiraiAdapter

--- adapters/mirai/adapter.py
+++ adapters/mirai/adapter.py
@@ -5,7 +5,7 @@ from JustBot.adapters.mirai.sender_handler import MiraiSenderHandler
 from JustBot.utils import Logger
 from JustBot.application import HTTP_PROTOCOL, WS_PROTOCOL
 
-from requests import get as sync_get
+from aiohttp import request
 
 
 class MiraiAdapter(Adapter):
@@ -27,23 +27,24 @@ class MiraiAdapter(Adapter):
         global_config.message_handler = self.message_handler
         global_config.adapter_utils = self.utils
 
-    def __request_api(self, api_path: str) -> str:
+    async def __request_api(self, api_path: str) -> str:
         try:
-            return sync_get(f'{HTTP_PROTOCOL}{self.http_host}:{self.http_port}{api_path}').json()
-        except ConnectionError as e:
+            async with request('GET', f'{HTTP_PROTOCOL}{self.http_host}:{self.http_port}{api_path}') as response:
+                return (await response.json())['data']
+        except ClientConnectorError as e:
             raise Exception(
                 f'无法连接到 Mirai-Api-Http, 请检查是否配置完整! {e}')
 
     @property
-    def login_info(self) -> dict:
+    async def login_info(self) -> dict:
         pass
 
     @property
-    def account(self) -> int:
+    async def account(self) -> int:
         pass
 
     @property
-    def nick_name(self) -> str:
+    async def nick_name(self) -> str:
         pass
 
     async def check(self) -> None:
