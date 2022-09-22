85b739c3d054705bc13cd5685ec32ccf0cb4e17d
WindLeaf233 <mc.windleaf@foxmail.com>
2022-09-23 00:17:01 +0800

    :sparkles: set default configs

--- jbot/adapters/cqhttp/config.py
+++ jbot/adapters/cqhttp/config.py
@@ -9,14 +9,14 @@ class CQHTTPConfig(AdapterConfig):
     > 说明
         CQHTTP 适配器配置类.
     > 参数
-        * ws_host [str]: WebSocket 主机地址
-        * ws_port [int]: WebSocket 主机端口
-        * http_host [str]: HTTP 主机地址
-        * http_port [int]: HTTP 主机端口
+        * ws_host [str]: WebSocket 主机地址 [default='127.0.0.1']
+        * ws_port [int]: WebSocket 主机端口 [default=6700]
+        * http_host [str]: HTTP 主机地址 [default='127.0.0.1']
+        * http_port [int]: HTTP 主机端口 [default=5700]
         * ws_reverse [bool]: 是否为反向 WebSocket [default=False]
     """
-    ws_host: str
-    ws_port: int
-    http_host: str
-    http_port: int
+    ws_host: str = '127.0.0.1'
+    ws_port: int = 6700
+    http_host: str = '127.0.0.1'
+    http_port: int = 5700
     ws_reverse: bool = False
